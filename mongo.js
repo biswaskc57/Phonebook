4a5ab2f63ff46e2985975da253d6089ac3e01d88
biswaskc57 <biswas.k.c@myy.haaga-helia.fi>
2021-06-02 18:39:59 +0300

    upto 3.18 done

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://Biswas123:${password}@cluster0.mq8ta.mongodb.net/persondb?retryWrites=true&w=majority`;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+});
+/*
+person.save().then((result) => {
+  console.log("added " + person.name + " number " + person.number);
+  mongoose.connection.close();
+});*/
+
+Person.find({}).then((result) => {
+  result.forEach((person) => {
+    console.log(person);
+  });
+  mongoose.connection.close();
+});
